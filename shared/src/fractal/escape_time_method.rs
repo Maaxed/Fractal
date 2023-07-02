6269025bba6ad75bc0b480f838c411a9c05fb776
Lucas Guedon <guedon.lucas@laposte.net>
2023-07-02 20:18:08 +0200

    Add periodicity checking

--- shared/src/fractal/escape_time_method.rs
+++ shared/src/fractal/escape_time_method.rs
@@ -4,6 +4,7 @@ pub fn compute_escape_time(iteration_count: u32, max_length: f64, pos: DVec2, se
 {
     let max_length_squared = max_length * max_length;
     let mut z = pos;
+    let mut prev_z = z;
     for i in 0..iteration_count
     {
         let length_squared = z.length_squared();
@@ -12,6 +13,18 @@ pub fn compute_escape_time(iteration_count: u32, max_length: f64, pos: DVec2, se
             return i as f32 / iteration_count as f32;
         }
         z = iteration_function(z, secondary_pos);
+
+        // Periodicity checking: check for cycles with previously saved z
+        if (z - prev_z).abs().cmplt(DVec2::splat(1.0e-20)).all()
+        {
+            return 1.0;
+        }
+
+        // Save z every 32 iteration
+        if i % 32 == 7
+        {
+            prev_z = z;
+        }
     }
 
     1.0
